c2a49717a1ddef5cf1d0e052e83baf88fa357de6
KatUser <ekaterina.shirokikh@gmail.com>
2024-04-08 08:14:16 +0700

    2. Создание и запуск нити [#1016]

--- src/main/java/concurrent/ConcurrentOutput.java
+++ src/main/java/concurrent/ConcurrentOutput.java
@@ -2,23 +2,22 @@ package concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
+
         Thread another = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        another.start();
-        another.run(); /* output : main ; Метод run не дает указания выполнить свои операторы в отдельной нити,
-        как это делаем метод Thread#start. Метод run напрямую вызывает операторы в той же нити, в которой запущен этот метод.*/
-        System.out.println(Thread.currentThread().getName());
 
-        /*
-        Thread another = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println(Thread.currentThread().getName());
-                    }
-                }
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
         );
-         */
+
+        Thread third = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        another.start();
+        second.start();
+        third.start();
+        System.out.println(Thread.currentThread().getName());
     }
 }
