9582f7a04ffe77a1d1e3eced390e76bee7f4fdeb
KatUser <ekaterina.shirokikh@gmail.com>
2024-04-09 15:50:45 +0700

    3. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,4 +1,27 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
+    public static void main(String[] args) {
+
+        Thread firstThread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        Thread secondThread = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        firstThread.start();
+        secondThread.start();
+
+        while (true) {
+            if (firstThread.getState() == Thread.State.TERMINATED
+                    && secondThread.getState() == Thread.State.TERMINATED) {
+                System.out.println(firstThread.getState());
+                System.out.println(secondThread.getState());
+                System.out.println("The end");
+                break;
+            }
+        }
+    }
 }
