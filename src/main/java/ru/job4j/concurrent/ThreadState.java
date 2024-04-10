c5750a73be199d32cd39033f53e451bd9b0042bb
KatUser <ekaterina.shirokikh@gmail.com>
2024-04-10 15:52:58 +0700

    3. Состояние нити.   [#229175 #490616]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,27 +1,24 @@
 package ru.job4j.concurrent;
-
 public class ThreadState {
     public static void main(String[] args) {
-
         Thread firstThread = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-
         Thread secondThread = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-
+        System.out.printf("%s %s%n", firstThread.getName(), firstThread.getState());
+        System.out.printf("%s %s%n", secondThread.getName(), secondThread.getState());
         firstThread.start();
         secondThread.start();
-
-        while (true) {
-            if (firstThread.getState() == Thread.State.TERMINATED
-                    && secondThread.getState() == Thread.State.TERMINATED) {
-                System.out.println(firstThread.getState());
-                System.out.println(secondThread.getState());
-                System.out.println("The end");
-                break;
-            }
+        while (firstThread.getState() != Thread.State.TERMINATED) {
+            System.out.printf("%s %s%n", firstThread.getName(), firstThread.getState());
         }
+        while (secondThread.getState() != Thread.State.TERMINATED) {
+            System.out.printf("%s %s%n", secondThread.getName(), secondThread.getState());
+        }
+        System.out.printf("%s %s%n", firstThread.getName(), firstThread.getState());
+        System.out.printf("%s %s%n", secondThread.getName(), secondThread.getState());
+        System.out.println("ALL THREADS ARE TERMINATED");
     }
 }
