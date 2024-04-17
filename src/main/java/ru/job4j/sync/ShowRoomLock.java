5331e264883afaf1f2e6dfa52fe19c51450bf877
KatUser <ekaterina.shirokikh@gmail.com>
2024-04-17 08:12:59 +0700

    1. Синхронизация общих ресурсов. [#1096]

--- src/main/java/ru/job4j/sync/ShowRoomLock.java
+++ src/main/java/ru/job4j/sync/ShowRoomLock.java
@@ -2,14 +2,16 @@ package ru.job4j.sync;
 
 public class ShowRoomLock {
     /* Монитор - это объект ShowRoomLock */
-    public void lockOfInstance() {
+    public Object lockOfInstance() {
         synchronized (this) {
+            return null;
         }
     }
 
     /* Монитор будет сам класс ShowRoomLock */
-    public static void lockOfClass() {
+    public static Object lockOfClass() {
         synchronized (ShowRoomLock.class) {
+            return null;
         }
     }
 }
