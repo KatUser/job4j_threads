b7bca0e4de2adb8aa4b1d1c4b511824b86a37884
KatUser <ekaterina.shirokikh@gmail.com>
2024-05-13 11:27:11 +0700

    2. ExecutorService рассылка почты.   [#63097 #495713]

--- src/main/java/ru/job4j/email/EmailNotification.java
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -1,23 +1,17 @@
 package ru.job4j.email;
 
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class EmailNotification {
     private final ExecutorService pool =  Executors.newFixedThreadPool(
                 Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
-        AtomicReference<String> subject = new AtomicReference<>(
-                String.format("Notification {%s} to email {%s}",
-                        user.username(),
-                        user.email())
-        );
-        AtomicReference<String> body = new AtomicReference<>(
-                String.format("Add a new event to {%s}%n",
-                        user.username())
-        );
-        pool.submit(() -> send(subject.get(), body.get(), user.email()));
+        String subject = String.format("Notification {%s} to email {%s}",
+                user.username(), user.email());
+        String body = String.format("Add a new event to {%s}",
+                user.username());
+        pool.submit(() -> send(subject, body, user.email()));
     }
 
     public void send(String subject, String body, String email) {
