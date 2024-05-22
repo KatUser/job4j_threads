b3cc78cdb9968aa08bb3cfab8092c58d818961c4
KatUser <ekaterina.shirokikh@gmail.com>
2024-05-22 08:01:38 +0700

    4. CompletableFuture [#361626 #496898]

--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.pools;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutionException;
@@ -9,29 +8,31 @@ import static org.assertj.core.api.Assertions.*;
 
 class RolColSumTest {
 
-    private int[][] matrix;
-
-    private Sums[] allExpected;
-
-    @BeforeEach
-    public void init() {
-        matrix =
-                new int[][]{{1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9}};
-        allExpected =
-                new Sums[]{new Sums(6, 12),
-                        new Sums(15, 15),
-                        new Sums(24, 18)};
-    }
-
     @Test
     void whenCalculatingSumsWithSync() {
+
+        int[][] matrix = new int[][]{{1, 2, 3},
+                                     {4, 5, 6},
+                                     {7, 8, 9}};
+
+        Sums[] allExpected = new Sums[]{new Sums(6, 12),
+                        new Sums(15, 15),
+                        new Sums(24, 18)};
+
         assertThat(RolColSum.sum(matrix)).containsExactly(allExpected);
     }
 
     @Test
     void whenCalculatingSumsWithAsync() throws ExecutionException, InterruptedException {
+
+        int[][] matrix = new int[][]{{1, 2, 3},
+                                     {4, 5, 6},
+                                     {7, 8, 9}};
+
+        Sums[] allExpected = new Sums[]{new Sums(6, 12),
+                        new Sums(15, 15),
+                        new Sums(24, 18)};
+
         assertThat(RolColSum.asyncSum(matrix)).containsExactly(allExpected);
     }
 }
